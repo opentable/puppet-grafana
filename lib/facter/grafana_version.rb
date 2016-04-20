17f0ff9774792967a445172001c96921e8773392
Maxime DEVALLAND <mdevalland@opentable.com>
2016-04-20 16:00:15 +0100

    fix issue no packages found matching grafana

--- lib/facter/grafana_version.rb
+++ lib/facter/grafana_version.rb
@@ -1,7 +1,9 @@
 if  FileTest.exists?("/usr/bin/dpkg-query")
-	Facter.add("grafana_version") do
-		setcode do
-			%x{/usr/bin/dpkg-query -W -f='${Version}' grafana}
+	if system("/usr/bin/dpkg -l | grep -q grafana")
+		Facter.add("grafana_version") do
+			setcode do
+				%x{/usr/bin/dpkg-query -W -f='${Version}' grafana}
+			end
 		end
 	end
 end
